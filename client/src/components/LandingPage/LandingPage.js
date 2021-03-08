82ec7d009ffe1ebe06b9f429a26196270048149a
KamajiiSC <brandonlschaen@gmail.com>
2021-03-08 14:23:43 -0500

    Imported Header to Landing Page

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './LandingPage.css';
+import Header from '../Header/Header';
 
 const LandingPage = () => (
   <section className="landing">
-  
+    <Header />
   </section>
 );
 
