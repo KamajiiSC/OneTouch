08c42f2cd0c36d60893ceda9a792f41d76d4039b
KamajiiSC <brandonlschaen@gmail.com>
2021-03-08 14:25:07 -0500

    Imported LandingPage to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <main className="App">
-      OneTouch
+      <LandingPage />
     </main>
   );
 }
